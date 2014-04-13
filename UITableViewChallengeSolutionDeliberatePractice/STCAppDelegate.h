fb0caa61cb83f5f25cd2d0731c99440a7b563f2e
StefenClausen <stefanclaussen@me.com>
2014-04-13 10:24:47 +0100

    Initial Commit

---
+++ UITableViewChallengeSolutionDeliberatePractice/STCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STCAppDelegate.h
+//  UITableViewChallengeSolutionDeliberatePractice
+//
+//  Created by Stefan Claussen on 13/04/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewChallengeSolutionDeliberatePractice/STCViewController.h
@@ -0,0 +1,13 @@
+//
+//  STCViewController.h
+//  UITableViewChallengeSolutionDeliberatePractice
+//
+//  Created by Stefan Claussen on 13/04/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCViewController : UIViewController
+
+@end
